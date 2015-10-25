5c5028c3093fed4916080539ac8d75a47327e8f4
Philip Xie <pxie@ucla.edu>
2015-10-25 01:06:49 -0400

    Dummy project for to test TestNG plugin

---
+++ pluginTest/src/pluginTest/dummyClass.java
@@ -0,0 +1,8 @@
+package pluginTest;
+
+public class dummyClass {
+
+	public void dummyMethod() {
+		System.out.println("hi!");
+	}
+}
---
+++ pluginTest/src/pluginTest/dummyClassTest.java
@@ -0,0 +1,11 @@
+package pluginTest;
+
+import org.testng.annotations.Test;
+
+public class dummyClassTest {
+
+  @Test
+  public void dummyMethod() {
+    System.out.println("Should work but there's a plugin error");
+  }
+}
